4d080c911458d1247743f8e15948ad92661bf560
vonLeebpl <vonleebpl@gmail.com>
2021-12-30 19:45:51 +0100

    0.1

--- VendorPriestNPCPatcher/Program.cs
+++ VendorPriestNPCPatcher/Program.cs
@@ -4,23 +4,63 @@ using System.Linq;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
+using Noggog;
+using VendorPriestNPCPatcher.Utilities;
 using System.Threading.Tasks;
+using Mutagen.Bethesda.Plugins;
+
 
 namespace VendorPriestNPCPatcher
 {
     public class Program
     {
+        const string PVendorPatchName = "Priest Vendors SE.esp";
         public static async Task<int> Main(string[] args)
         {
             return await SynthesisPipeline.Instance
-                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
-                .SetTypicalOpen(GameRelease.SkyrimSE, "YourPatcher.esp")
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch, new PatcherPreferences()
+                {
+                    ExclusionMods = new List<ModKey>()
+                    {
+                         new ModKey(PVendorPatchName, ModType.Plugin),
+                         new ModKey("Nemesis PCEA.esp", ModType.Plugin)
+                    }
+                })
+                .SetTypicalOpen(GameRelease.SkyrimSE, "VendorPriestNPCPatcher.esp")
                 .Run(args);
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            //Your code here!
+            var VPriest = state.LoadOrder.GetModByFileName("Priest Vendors SE.esp");
+
+            if (VPriest == null)
+            {
+                System.Console.WriteLine("Priest Vendors SE.esp not found.");
+                return;
+            }
+
+            var VPriestFormIDs = VPriest.Npcs.Select(x => x.FormKey).ToList();
+            var winningOverrides = state.LoadOrder.PriorityOrder.WinningOverrides<INpcGetter>().Where(x => VPriestFormIDs.Contains(x.FormKey)).ToList();
+
+            foreach (var npc in VPriest.Npcs)
+            {
+                if (new List<uint>() { 7, 14 }.Contains(npc.FormKey.ID)) continue; // ignore player record
+
+                var winningOverride = winningOverrides.Where(x => x.FormKey == npc.FormKey).First();
+
+                var patchNpc = state.PatchMod.Npcs.GetOrAddAsOverride(winningOverride);
+                foreach (var fac in npc.Factions)
+                    if (!patchNpc.Factions.Select(x => new KeyValuePair<FormKey, int>(x.Faction.FormKey, x.Rank)).Contains(new KeyValuePair<FormKey, int>(fac.Faction.FormKey, fac.Rank)))
+                    {
+                        patchNpc.Factions.Add(fac.DeepCopy());
+                    }
+            }
+
+            if (state.PatchMod.ModKey.Name == PVendorPatchName)
+            {
+                state.PatchMod.ModHeader.Flags = state.PatchMod.ModHeader.Flags | SkyrimModHeader.HeaderFlag.LightMaster;
+            }
         }
     }
 }
---
+++ VendorPriestNPCPatcher/Utilities/ExLoadOrderUtilities.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using System.Text;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Order;
+using Mutagen.Bethesda.Plugins.Records;
+
+namespace VendorPriestNPCPatcher.Utilities
+{
+    public static class ExLoadOrderUtilities
+    {
+        public static ISkyrimModGetter? GetModByFileName(this ILoadOrder<IModListing<ISkyrimModGetter>>? LoadOrder, string Name)
+        {
+            if (LoadOrder == null) return null;
+            var Mods = LoadOrder.Keys.Where(x => ((string)x.FileName).ToLower() == Name.ToLower()).ToList();
+            return (Mods.Count > 0) ? LoadOrder[Mods.First()].Mod : null;
+        }
+
+
+        public static int GetFileOrder(this ILoadOrder<IModListing<ISkyrimModGetter>>? LoadOrder, string Filename)
+        {
+            if (Filename == null || LoadOrder == null) return -1;
+            return LoadOrder.Keys.ToList().FindIndex(0, LoadOrder.Keys.Count(), x => ((string)x.FileName).ToLower() == Filename.ToLower());
+        }
+
+        public static int GetOrder<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        {
+            if (record == null || state == null) return -1;
+
+            return state.LoadOrder.HasMod(record.FormKey.ModKey, true) ? state.LoadOrder.ListedOrder.Select(x => x.ModKey).ToList().IndexOf(record.FormKey.ModKey) : -1;
+        }
+    }
+}
